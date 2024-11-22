1c5eaa4192fb3a820d07a39add5b6bb89e93bed6
Sanya Sahu <155381474+sahusanya@users.noreply.github.com>
2024-11-22 21:29:50 +0530

    Add files via upload
    post order and in order done

--- tree.cc
+++ tree.cc
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+//recursive tree traversal used
 
 struct tree {
     int data;
@@ -9,13 +10,41 @@ struct tree {
     tree(int val):(data(val), left(nullptr), right(nullptr)) {}
 };
 
+
 void insertData(int data) {
 
 }
 
-void preOrder(tree* node, vector<int> & result )  {
+void preOrder(tree* node, vector<int> &result)  {
   if (node == nullptr) return;
   result.push_back(node->data);
   preOrder(node->left, result);
   preOrder(node->right, result);
+}
+
+void inOrder(tree* node, vector<int> &result) {
+  if (node == nullptr) return;
+  preOrder(node->left, result);
+  result.push_back(node->data);
+  preOrder(node->right, result);
+}
+
+void postOrder(tree* node, vector<int> &result) {
+  if (node == nullptr) return;
+
+  preOrder(node->left, result);
+  preOrder(node->right, result);
+  result.push_back(node->data);
+}
+int main() {
+  vector<int> result;
+  struct tree* root = new tree(1);
+  root->left = new tree(2);
+  root->rigth = new tree(3);
+  root->left->left = new tree(4);
+  root->rigth->rigth = new tree(7);
+  preOrder(root, result);
+  for (int i : result) {
+    cout<<i<<" ";
+  }
 }
